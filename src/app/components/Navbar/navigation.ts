326c8f7771226c81b25481d843cbcd938747079e
Antonio <antonioeprado18@gmail.com>
2024-04-08 14:16:44 -0300

    feat: navbar initial design

---
+++ src/app/components/Navbar/navigation.ts
@@ -0,0 +1,130 @@
+export const navigation = {
+	categories: [
+		{
+			id: 'food',
+			name: 'Food',
+			featured: [
+				{
+					name: 'New Arrivals',
+					href: '#',
+					imageSrc:
+						'https://nypost.com/wp-content/uploads/sites/2/2022/10/dogfood.jpg?quality=75&strip=all',
+					imageAlt: 'Petline pet food.',
+				},
+				{
+					name: 'Dog food',
+					href: '#',
+					imageSrc:
+						'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPapRe8riaU8P_QDs_D-OFzcZumIVx1ajnJY18ppXaIA&s',
+					imageAlt: 'Dog food',
+				},
+			],
+			sections: [
+				{
+					id: 'dogs',
+					name: 'Dogs',
+					items: [
+						{ name: 'Dry Food', href: '#' },
+						{ name: 'Premium Food', href: '#' },
+						{ name: 'Wet Food', href: '#' },
+						{ name: 'Food Toppings', href: '#' },
+						{ name: 'Raw Food', href: '#' },
+						{ name: 'Veterinary Diets', href: '#' },
+						{ name: 'Fresh & Prepared Meals', href: '#' },
+						{ name: 'Dehydrated Food', href: '#' },
+						{ name: 'Browse All', href: '#' },
+					],
+				},
+				{
+					id: 'Cats',
+					name: 'Cats',
+					items: [
+						{ name: 'Dry Food', href: '#' },
+						{ name: 'Premium Food', href: '#' },
+						{ name: 'Wet Food', href: '#' },
+						{ name: 'Food Toppings', href: '#' },
+						{ name: 'Raw Food', href: '#' },
+						{ name: 'Veterinary Diets', href: '#' },
+						{ name: 'Fresh & Prepared Meals', href: '#' },
+						{ name: 'Dehydrated Food', href: '#' },
+						{ name: 'Browse All', href: '#' },
+					],
+				},
+				{
+					id: 'brands',
+					name: 'Brands',
+					items: [
+						{ name: 'Pedigree', href: '#' },
+						{ name: 'Blue Buffalo', href: '#' },
+						{ name: "Hill's", href: '#' },
+						{ name: 'Purina', href: '#' },
+						{ name: "Kibbles 'n Bits", href: '#' },
+					],
+				},
+			],
+		},
+		{
+			id: 'health',
+			name: 'Health',
+			featured: [
+				{
+					name: 'New Arrivals',
+					href: '#',
+					imageSrc:
+						'https://image.chewy.com/is/image/catalog/366899_MAIN._AC_SS232_V1645037798_.jpg',
+					imageAlt: 'Blundle Nexgard+Heartgard',
+				},
+				{
+					name: 'Chews for Dogs',
+					href: '#',
+					imageSrc:
+						'https://image.chewy.com/is/image/catalog/146066_MAIN._AC_SS232_V1670367424_.jpg',
+					imageAlt: 'Chews for Dogs',
+				},
+			],
+			sections: [
+				{
+					id: 'dogs',
+					name: 'Dogs',
+					items: [
+						{ name: 'Flea & Tick', href: '#' },
+						{ name: 'Heartworm', href: '#' },
+						{ name: 'Pain Relief & Arthritis', href: '#' },
+						{ name: 'Antibiotics', href: '#' },
+						{ name: 'Alergy Relief', href: '#' },
+						{ name: 'Diabetes Care & Insulin', href: '#' },
+						{ name: 'Browse All', href: '#' },
+					],
+				},
+				{
+					id: 'cats',
+					name: 'Cats',
+					items: [
+						{ name: 'Flea & Tick', href: '#' },
+						{ name: 'Dewormers & Parasiticides', href: '#' },
+						{ name: 'Antibiotics', href: '#' },
+						{ name: 'Urinary Tract & Kidneys', href: '#' },
+						{ name: 'Pain Relief & Arthritis', href: '#' },
+						{ name: 'Diabetes Care & Insulin', href: '#' },
+						{ name: 'Browse All', href: '#' },
+					],
+				},
+				{
+					id: 'brands',
+					name: 'Brands',
+					items: [
+						{ name: 'Simparica Trio', href: '#' },
+						{ name: 'Apoquel', href: '#' },
+						{ name: 'Heartgard', href: '#' },
+						{ name: 'NexGard', href: '#' },
+						{ name: 'Bravecto', href: '#' },
+					],
+				},
+			],
+		},
+	],
+	pages: [
+		{ name: 'Company', href: '#' },
+		{ name: 'Stores', href: '#' },
+	],
+};
