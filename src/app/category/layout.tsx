574e1c2cca79304ff0f7058aba0d7209af63123f
Antonio <antonioeprado18@gmail.com>
2024-04-09 09:11:39 -0300

    moved dynamicParams from file

--- src/app/category/layout.tsx
+++ src/app/category/layout.tsx
@@ -4,8 +4,6 @@ type Props = {
 	children: React.ReactNode;
 };
 
-export const dynamicParams = false;
-
 export default function CategoryLayout({ children }: Props) {
 	return <main>{children}</main>;
 }
