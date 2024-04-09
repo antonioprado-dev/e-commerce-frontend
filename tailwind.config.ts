74e56fc981013aede05061ef17f966b65fe2ace2
Antonio <antonioeprado18@gmail.com>
2024-04-09 09:10:57 -0300

    added necessary configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
 				'gradient-conic':
 					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
 			},
+			gridTemplateRows: {
+				'[auto,auto,1fr]': 'auto auto 1fr',
+			},
 		},
 	},
 	plugins: [require('@tailwindcss/aspect-ratio')],
